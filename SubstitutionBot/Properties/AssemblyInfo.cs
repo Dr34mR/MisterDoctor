3c7b2b23c8b53d25db3e97a647537c0768ba0870
Scott Loomes <loomesno1@gmail.com>
2019-11-05 19:34:07 +1000

    1.0.7248.35203

--- SubstitutionBot/Properties/AssemblyInfo.cs
+++ SubstitutionBot/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.7247.41579")]
-[assembly: AssemblyFileVersion("1.0.7247.41579")]
+[assembly: AssemblyVersion("1.0.7248.35203")]
+[assembly: AssemblyFileVersion("1.0.7248.35203")]
