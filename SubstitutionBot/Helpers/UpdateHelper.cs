740bc44a11005080766404004afe7a90abefa66c
Scott Loomes <loomesno1@gmail.com>
2019-11-05 20:39:08 +1000

    Version.txt

---
+++ SubstitutionBot/Helpers/UpdateHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstitutionBot.Helpers
+{
+    internal static class UpdateHelper
+    {
+        internal static bool UpdateAvailable()
+        {
+            try
+            {
+                var vlient = new WebClient();
+                var theTextFile = vlient.DownloadString("The Direct Address");
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        internal static bool LaunchUrl()
+        {
+
+        }
+    }
+}
